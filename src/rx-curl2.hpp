7ae68b73d4ed7875fe952a6799832fb46198877a
MH Alikhani <mhalikhani@DESKTOP-DKVSOC3.localdomain>
2022-09-05 14:37:03 +0430

    Add support for GET PUT PATCH DELETE Requests.
    minor improvment.

--- src/rx-curl2.hpp
+++ src/rx-curl2.hpp
@@ -27,17 +27,26 @@ struct HttpResponse
 	int http_status;
 };
 
-size_t write_to_string(void *contents, size_t size, size_t nmemb, void *userp)
+size_t write_callback(void *contents, size_t size, size_t nmemb, void *userp)
 {
 	((std::string*)userp)->append((char*)contents, size * nmemb);
     return size * nmemb;
+}
+
+static size_t read_callback(void *ptr, size_t size, size_t nmemb, void *stream)
+{
+	curl_off_t nread = strlen((const char *)stream);
+	memcpy(ptr, stream, nread);
+	return nread;
 }
 
 struct RxCurl2
 {
     rxcpp::observable<HttpResponse> create(HttpRequest request)
     {
-		return rxcpp::observable<>::create<HttpResponse>([=](rxcpp::subscriber<HttpResponse> &out)
+		
+		
+		return rxcpp::observable<>::create<HttpResponse>([&](rxcpp::subscriber<HttpResponse> &out)
 		{
 			std::future<HttpResponse> result
 			{
@@ -53,35 +62,63 @@ struct RxCurl2
 					{
 						curl_easy_setopt(curl, CURLOPT_URL, request.url.c_str());
 						curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response.response_body);
-						curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_to_string);
+						curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
 
-						if (request.method == HttpRequestMethod::kPost)
+						switch(request.method)
 						{
-							curl_easy_setopt(curl, CURLOPT_POST, 1L);
-							curl_easy_setopt(curl, CURLOPT_POSTFIELDS, request.body.c_str());
+							case HttpRequestMethod::kGet:
+							{
+								// For future use.
+							}
+							break;
+							case HttpRequestMethod::kPost:
+							{
+								curl_easy_setopt(curl, CURLOPT_POST, 1L);
+								curl_easy_setopt(curl, CURLOPT_POSTFIELDS, request.body.c_str());
+							}
+							break;
+							case HttpRequestMethod::kPatch:
+							{
+								curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST,"PATCH");
+								curl_easy_setopt(curl, CURLOPT_POSTFIELDS,request.body.c_str());
+							}
+							break;
+							case HttpRequestMethod::kPut:
+							{
+								curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
+								curl_easy_setopt(curl, CURLOPT_READDATA, request.body.c_str());
+								curl_easy_setopt(curl, CURLOPT_READFUNCTION, read_callback);
+								curl_easy_setopt(curl, CURLOPT_INFILESIZE, strlen(request.body.c_str()));
+							}
+							break;
+							case HttpRequestMethod::kDelete:
+							{
+								curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST,"DELETE");
+								curl_easy_setopt(curl, CURLOPT_POSTFIELDS,request.body.c_str());
+							}
+							break;
 						}
 
-						struct curl_slist *header_list = NULL;
-						std::string headerString;
-
-						for (auto header = request.headers.begin(); header != request.headers.end(); ++header)
-						{ 	
-        					headerString = header->first;
-        					headerString.append(": ").append(header->second);
-       						header_list = curl_slist_append(header_list, headerString.c_str()); 
-    					}
-						curl_easy_setopt(curl, CURLOPT_HTTPHEADER, header_list);
+						struct curl_slist *headers;
+						std::vector<std::string> strings;
+						for (auto& h : request.headers)
+						{
+							strings.push_back(h.first + ": " + h.second);
+							headers = curl_slist_append(headers, strings.back().c_str());
+						}
+						if (!!headers)
+						{
+							curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+						}
 
 						curl_easy_setopt(curl, CURLOPT_USERAGENT, "PostmanRuntime/7.29.0");
 						curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
 						curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 1L);
 						curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1);
 
-						res = curl_easy_perform(curl);
-						curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response.http_status);
-
-						response.code = res;
+						response.code = curl_easy_perform(curl);
 						response.error = curl_easy_strerror(res);
+						curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response.http_status);
 
 						curl_easy_cleanup(curl);
 					}
