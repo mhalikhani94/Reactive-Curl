753c7b798ade1e5b3816fa3c0ea0c7de4c793e98
MH Alikhani <mhalikhani@DESKTOP-DKVSOC3.localdomain>
2022-09-07 15:56:25 +0430

    Add HttpClient Version 0.0.1
    unused files deleted.
    http-client added to the project.
    cmake file updated.

--- src/reactive-curl.cpp
+++ src/reactive-curl.cpp
@@ -7,29 +7,23 @@ using json = nlohmann::json;
 #include <iostream>
 #include <chrono>
 #include <thread>
-#include "rx-curl2.hpp"
-#include "mh-http-client.hpp"
+#include "http-client/http-client.hpp"
 
 int main()
 {
-    const auto get_url = "www.google.com";
+    const auto get_url = "https://httpbin.org/get";
     const auto post_url = "https://httpbin.org/post";
 
-    restincurl::Client client;
-    // client.Build()->Get("www.google.com")
-    // .Option(CURLOPT_VERBOSE, 1L)
-    // .WithCompletion([&](const restincurl::Result& result) {
-    //    std::cout << result.body << std::endl;
-    // })
-    // .ExecuteSynchronous();
-
-    client.Build()->Post("https://mh-rx-test.free.beeceptor.com")
-        .AcceptJson()
-        .WithCompletion([&](const restincurl::Result& result) {
-            if (result.isOk()) {
-                try {
-                    const auto j1 = json::parse(result.body);
-                    std::cout << j1.dump() << std::endl;
+    HttpClient client;
+    client.build()->Get(get_url)
+    .add_curl_option(CURLOPT_VERBOSE, 0L)
+    .process_response([&](const HttpResponse& result)
+    {
+       if (result.is_ok())
+            {
+                try
+                {
+                    std::cout << "Result Body: " << result.body << std::endl;
                     std::cout << "Result headers: " << result.header << std::endl;
                 } catch (const std::exception& ex) {
                     std::cout << ex.what() << std::endl;
@@ -37,61 +31,40 @@ int main()
                 }
                 
             } else {
-                std::cout << "Request failed: " << result.msg << std::endl
+                std::cout << "Request failed: " << result.error_msg << std::endl
+                    << "HTTP code: " << result.http_response_code << std::endl;
+            }
+    })
+    .send_request();
+
+    client.build()->Post(post_url)
+        .accept_json()
+        .add_json_body("")
+        .add_header("Cache-Control: no-cache, no-store, must-revalidate")
+        .add_header("Pragma: no-cache")
+        .add_header("Expires: 0")
+        .process_response([&](const HttpResponse& result)
+        {
+            if (result.is_ok())
+            {
+                try
+                {
+                    std::cout << "Result Body: " << result.body << std::endl;
+                    std::cout << "Result headers: " << result.header << std::endl;
+                } catch (const std::exception& ex) {
+                    std::cout << ex.what() << std::endl;
+                    return;
+                }
+                
+            } else {
+                std::cout << "Request failed: " << result.error_msg << std::endl
                     << "HTTP code: " << result.http_response_code << std::endl;
             }
         })
-        .ExecuteSynchronous();
-
-/*
-    HttpClient* client = new HttpClient.
-    client.post("URL")
-        .add_headers("Header1")
-        .add_headers("Header2")
-        .add_headers("Header3")
-        .add_body("body1")
-        .add_body("body2")
-        .build();
-// */
-
-
-
-
-
-
-
-
-
-
-
-    // auto rx_curl2_instance = create_rxcurl2();
-    // rx_curl2_instance->create(HttpRequest{post_url, HttpRequestMethod::kPost, {}, {}}).subscribe(
-    //     [=](const HttpResponse& r)
-    //     {
-    //         std::cout << r.response_body << std::endl;
-    //         exit(0);
-    //     }, [] ()
-    //     {
-    //     }
-    // );
-
-    // rx_curl2_instance->create(HttpRequest{get_url, HttpRequestMethod::kGet, {}, {}}).subscribe(
-    //     [=](const HttpResponse& r)
-    //     {
-    //         std::cout << r.response_body << std::endl;
-    //         exit(0);
-    //     }, [] ()
-    //     {
-    //     }
-    // );
+        .send_request();
 
     while (true)
     {
         std::this_thread::sleep_for(std::chrono::hours(1));
     }
 }
-
-//subscibe_on(sceduler)
-//observe_on(computation)
-////mapping//observe_on(main)
-//subscribe on main
--- src/types.hpp
+++ src/types.hpp
@@ -1,64 +1,22 @@
-#ifndef REACTIVE_CURL_HPP
-#define REACTIVE_CURL_HPP
+#ifndef REACTIVE_CURL_TYPES_HPP
+#define REACTIVE_CURL_TYPES_HPP
 
 #include <curl/curl.h>
 
 #include <string>
 #include <map>
-
-
-#include <algorithm>
-#include <atomic>
-#include <deque>
-#include <exception>
-#include <functional>
-#include <iostream>
-#include <iterator>
-#include <map>
-#include <memory>
-#include <mutex>
-#include <string>
-#include <thread>
-#include <vector>
-
 #include <assert.h>
-#include <curl/curl.h>
-#include <curl/easy.h>
-#include <fcntl.h>
-#include <string.h>
 
-
-
-enum class RequestType { GET, PUT, POST, HEAD, DELETE, PATCH, OPTIONS, INVALID };
-
-using completion_fn_t = std::function<void (const Result& result)>;
-
-using HeaderFileds = std::map<std::string, std::string>;
-
-enum class HttpRequestMethod
+enum class RequestType
 {
 	kGet,
-	kPost,
-	kPatch,
 	kPut,
+	kPost,
+	kHead,
 	kDelete,
+	kPatch,
+	kOptions,
+	kInvalid
 };
 
-struct HttpRequest
-{
-	std::string url;
-	HttpRequestMethod method;
-	std::map<std::string, std::string> headers;
-	std::string body;
-};
-
-struct HttpResponse
-{
-	std::string body;
-	HeaderFileds headers;
-    std::string error;
-    int curl_code;
-	int http_code;
-};
-
-#endif //REACTIVE_CURL_HPP
+#endif //REACTIVE_CURL_TYPES_HPP
